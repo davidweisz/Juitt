98a64b1cb5fe7afbb9c73dddb39f91cd6eddba35
David Weisz <daweisz@gmail.com>
2012-02-04 19:34:34 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juitt::Application.config.secret_token = 'ae003416863bcb80aac2220ee70d274239817c64642f9493ade70190bd491f1611a32b69f636b86e6db316715dd7e1f68f9f078561ed87cc8d943c752c29bf43'
